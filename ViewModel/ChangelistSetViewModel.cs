87eeb13a1330ca9afe1d55b5bd791f06466e60ad
Simon Taylor <simontaylor81@hotmail.com>
2014-01-23 13:07:43 +0000

    Initial add

---
+++ ViewModel/ChangelistSetViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerforceChangelistViewer.ViewModel
+{
+    class ChangelistSetViewModel : ReactiveObject
+    {
+        // List of changelists in the set.
+        private ReactiveList<ChangelistViewModel> _changelists;
+        public ReactiveList<ChangelistViewModel> Changelists
+        {
+            get { return _changelists; }
+            set { this.RaiseAndSetIfChanged(ref _changelists, value); }
+        }
+    }
+}
---
+++ ViewModel/ChangelistViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerforceChangelistViewer.ViewModel
+{
+    class ChangelistViewModel : ReactiveObject
+    {
+        // The changelist number.
+        private int _number;
+        public int Number
+        {
+            get { return _number; }
+            set { this.RaiseAndSetIfChanged(ref _number, value); }
+        }
+    }
+}
